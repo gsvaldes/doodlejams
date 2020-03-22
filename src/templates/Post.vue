f6729650815da62dc824803f4a00be1ea1dbcbff
valdesgs <gvaldes@austin.utexas.edu>
2020-03-22 15:56:49 -0500

    limit width of images

--- src/templates/Post.vue
+++ src/templates/Post.vue
@@ -1,11 +1,11 @@
 <template>
     <Layout>
-        <div class="postPage">
-            <h1 class="postPage__title">{{$page.post.title}}</h1>
+        <div class="post">
+            <h1 class="post__title">{{$page.post.title}}</h1>
             <p>{{$page.post.author}}</p>
             <p>{{$page.post.date}}</p>
-            <div class="postPage_img">
-                <g-image :src="$page.post.image" width="500"/>
+            <div class="post__img">
+                <g-image :src="$page.post.image"/>
             </div>
             <div v-html="$page.post.content"></div>
         </div>
@@ -38,6 +38,16 @@ export default {
 h2 {
   padding-top: .8em;
   padding-bottom: .6em;
+}
+
+.post__img {
+  width: 100%;
+  text-align: center;
+}
+
+.post__img img {
+  width: 100%;
+  padding: 2em;
 }
 
 </style>
