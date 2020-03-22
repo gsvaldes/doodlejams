6cd61b86292b2bb263ecd33123631e3e790bf867
valdesgs <gvaldes@austin.utexas.edu>
2020-03-22 15:10:26 -0500

    downgrade version of transformer-remark

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -4,7 +4,7 @@
     <!-- Learn how to use images here: https://gridsome.org/docs/images -->
     <!-- <g-image alt="Example image" src="~/favicon.png" width="135" /> -->
 
-    <post v-for="edge in $page.posts.edges" :key="edge.node.id">
+    <post v-for="edge in $page.allPost.edges" :key="edge.node.id">
       <h2><g-link :to="edge.node.path">{{edge.node.title}}</g-link></h2>
       <div class="article-meta">{{edge.node.date }}</div>
       <p>{{edge.node.abstract}}</p>
@@ -19,7 +19,7 @@
 
 <page-query>
 query {
-  posts: allPost {
+  allPost {
     edges {
       node {
         title
